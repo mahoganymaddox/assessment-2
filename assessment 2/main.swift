e31f29c0d06995d8ddbd98b110e005779bb38039
Mahogany Maddox <mahoganymaddox@Interapts-MacBook-Pro-76.local>
2018-02-07 14:13:08 -0500

    finished assessment

---
+++ assessment 2/main.swift
@@ -0,0 +1,59 @@
+//
+//  main.swift
+//  assessment 2
+//
+//  Created by Mahogany Maddox on 2/7/18.
+//  Copyright © 2018 Mahogany Maddox. All rights reserved.
+//
+
+import Foundation
+
+//1. Create a program that asks the user to input their first name and last name. Store their input as a variable and then print the input back to the user.
+var userInput = readLine()!
+print("what is your first name?")
+print("what is your last name?")
+//2. Create a variable called number
+var number = 75
+for i in 0...75 {
+print(75)
+//   we want to check if the variable number is between 1 and 100. Using if and else if statments. If a number is lower than one print "too low" to the console. If a number is higher than 100 print "too high" to the console. If a number is between 1 and 100 then print "It's right". Feel free to change the value of number to test your solution.
+//3. Using a for loop create a program that asks the user for 5 numbers then add those numbers together and return it to the users.
+/*  Example: "Input number 1"
+ 2
+ "Input number 2"
+ 2
+ "Input number 3"
+ 2
+ "Input number 4"
+ 2
+ "Input number 5"
+ 2
+ "Your total was 10!"
+     */
+    var number = Int()
+    for i in 0...100 {
+        print(i)
+    }
+    if number {
+        print("it's right")
+    }
+    else if < 1{
+        print("too low")
+    }
+    else if > 1 {
+        print("too high")
+    }
+            
+        }
+    
+
+//4. Give a brief explantion on how the different types of loops work (for, while, repeat)
+//for loops set a statement until proven false. while evaluates its condition at the start of each pass through the loop. repeat loops perform a single pass through.
+//5. BONUS: Change this code so that it uses ternary rather than an if statement.
+var boolInt = true
+    trueOrFalse == true ? print("it's true") : print("it's false")
+    print("Congratulations! You got the bonus question!") : print("sorry you did not pass the bonus question.")
+
+
+
+
